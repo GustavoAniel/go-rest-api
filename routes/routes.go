696b02338d7f1a0a2f7830cc4619c241af701176
GustavoAniel <aniel.amtimil@yahoo.com.br>
2023-07-31 10:30:14 -0300

    Versão 1.0.0

--- routes/routes.go
+++ routes/routes.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gustavoaniel/go-api-rest/controllers"
+	"github.com/gustavoaniel/go-api-rest/middleware"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidade", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidade/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidade", controllers.CriaUmaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidade/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidade/{id}", controllers.EditaUmaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
